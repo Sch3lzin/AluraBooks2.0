7a960e3f6d36014655afda3b64055233ac0ca269
Daniel Schefer <danielschefer2007@hotmail.com>
2025-07-28 17:12:00 -0300

    Finished project - 1.0

--- scripts/matodoFilter.js
+++ scripts/matodoFilter.js
@@ -4,6 +4,26 @@ btns.forEach(btn => btn.addEventListener("click", filtrarLivros));
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
-    let livrosFiltrados = categoria == "disponivel" ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == "disponivel" ? filtrarPorDisponibilidade() : FiltrarPorCategoria(categoria);
     exibirOsLivrosNaTela(livrosFiltrados);
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotal(livrosFiltrados);
+        exibirValor(valorTotal);
+    }
+}
+
+function FiltrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValor(valorTotal) {
+    elementoSection.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+        </div>
+    `;
 }
