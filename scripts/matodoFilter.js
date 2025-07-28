b164a89cfb2461a4d00885a0e6ca37b62c7690dd
Daniel Schefer <danielschefer2007@hotmail.com>
2025-07-28 12:46:30 -0300

    0.3 ( search filter added )

---
+++ scripts/matodoFilter.js
@@ -0,0 +1,9 @@
+const btns = document.querySelectorAll(".btn");
+btns.forEach(btn => btn.addEventListener("click", filtrarLivros));
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value;
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    exibirOsLivrosNaTela(livrosFiltrados);
+}
